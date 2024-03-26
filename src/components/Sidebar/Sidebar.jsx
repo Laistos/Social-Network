d5569fc16f88bfe567377da69c8fbb80511ed498
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2024-03-26 20:32:48 +0500

    added NavLink
    added Nav Link and stylization of links to other pages

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,27 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Sidebar.module.css'
 
 function Sidebar() {
    return (
       <nav className={styles.sidebar}>
          <div className={styles.listWrapper}>
-            <div><a className={styles.link} href='/profile'>Profile</a></div>
-            <div><a className={styles.link} href='/messages'>Messages</a></div>
-            <div><a className={styles.link} href='/music'>Music</a></div>
-            <div><a className={styles.link} href='/news'>News</a></div>
-            <div><a className={styles.link} href='/settings'>Settings</a></div>
+            <div className={styles.link}>
+               <NavLink className={({ isActive }) => isActive ? styles.active : styles.link} to='/profile' >Profile</NavLink>
+            </div>
+            <div className={styles.link}>
+               <NavLink className={({ isActive }) => isActive ? styles.active : styles.link} to='/messages' >Messages</NavLink>
+            </div>
+            <div className={styles.link}>
+               <NavLink className={({ isActive }) => isActive ? styles.active : styles.link} to='/music' >Music</NavLink>
+            </div>
+            <div className={styles.link}>
+               <NavLink className={({ isActive }) => isActive ? styles.active : styles.link} to='/news'>News</NavLink>
+            </div>
+            <div className={styles.link}>
+               <NavLink className={({ isActive }) => isActive ? styles.active : styles.link} to='/settings'>Settings</NavLink>
+            </div>
          </div>
-      </nav>
+      </nav >
    )
 }
 
