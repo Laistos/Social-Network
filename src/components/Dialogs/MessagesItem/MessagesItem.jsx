c917d4a651574cd0ae2af4536d24aee9e7000b5f
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2024-04-02 21:30:39 +0500

    added React ref

--- src/components/Dialogs/MessagesItem/MessagesItem.jsx
+++ src/components/Dialogs/MessagesItem/MessagesItem.jsx
@@ -1,7 +1,22 @@
+import React from 'react'
 import styles from './MessagesItem.module.css'
 
 export const MessagesItem = (props) => {
+
+   let newMessageElement = React.createRef()
+
+   let addMessage = () => {
+      let text = newMessageElement.current.value
+      alert(text)
+   }
+
    return (
-      <div className={styles.message}>{props.message}</div>
+      <div className={styles.message}>
+         <div className={styles.message_text}>{props.message}</div>
+         <div className={styles.add_message}>
+            <textarea ref={newMessageElement} className={styles.textarea} name="" id="" cols="30" rows="10">some text</textarea>
+            <button onClick={addMessage} className={styles.send_message}>Send</button>
+         </div>
+      </div>
    )
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,14 +1,23 @@
+import React from 'react'
 import styles from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = (props) => {
    let posts = props.post.map((post) => { return <Post message={post.message} likes={post.likes} /> })
+
+   let newPostElement = React.createRef() //создаем ссылку на элемент
+
+   let addPost = () => {
+      let text = newPostElement.current.value
+      alert(text)
+   }
+
    return (
       <div className={styles.wrapper}>
          MyPosts
          <div className={styles.addPost}>
-            <textarea className={styles.textarea}>New Post</textarea>
-            <button className={styles.button}>Add Post</button>
+            <textarea className={styles.textarea} ref={newPostElement} >New Post</textarea>     {/*newPostElement это ссылка на textarea*/}
+            <button className={styles.button} onClick={addPost} >Add Post</button>
          </div>
          {posts}
       </div>
