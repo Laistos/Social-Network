f66688e8a84105178d667c26fd428e643c72ba42
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2024-04-23 23:03:54 +0500

    added axios, made get request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,27 +1,37 @@
 import styles from './Users.module.css'
 import React from 'react'
+import axios from "axios";
+import avatar from '../../images/user-avatar/avatar.jpg'
 
 const Users = (props) => {
+
+    if (props.users.length === 0){
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+            props.setUsers(response.data.items)
+        })
+    }
+
+
     return (
         <div className={styles.wrapper}>
             {
                 props.users.map((u) => <div className={styles.wrapper} key={u.id}>
                         <div className={styles.flex_container}>
                             <div className={styles.avatar}>
-                                <img className={styles.image} src={u.avatar} alt="avatar"/>
+                                <img className={styles.image} src={u.photos.small != null ? u.photos.small : avatar} alt="avatar"/>
                             </div>
                             {u.followed
-                                ? <button onClick={() => {props.unfollow(u.id)}} className={styles.button}>Unfollow</button>
-                                : <button onClick={() => {props.follow(u.id)}} className={styles.button}>Follow</button>}
+                                ? <button onClick={() => {
+                                    props.unfollow(u.id)
+                                }} className={styles.button}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(u.id)
+                                }} className={styles.button}>Follow</button>}
                         </div>
                         <div className={styles.grid_container}>
                             <div className={styles.item_1}>
                                 <div className={styles.name}>{u.name}</div>
                                 <div className={styles.name}>{u.status}</div>
-                            </div>
-                            <div className={styles.item_2}>
-                                <div>{u.location.country}</div>
-                                <div>{u.location.city}</div>
                             </div>
                         </div>
                     </div>
--- src/redux/reducer/usersReducer.js
+++ src/redux/reducer/usersReducer.js
@@ -3,12 +3,7 @@ const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET-USERS'
 
 let initialState = {
-    users: [
-        {id: 1, followed: false, avatar: 'https://i.pinimg.com/564x/b2/81/ff/b281ff729f1b0b37d37d0d4400635912.jpg', name: 'Akezhan', status: 'i love React', location: {country: 'Kazakhstan', city: 'Almaty'}},
-        {id: 2, followed: false, avatar: 'https://i.pinimg.com/564x/b2/81/ff/b281ff729f1b0b37d37d0d4400635912.jpg', name: 'Alisher', status: 'i love R6', location: {country: 'Kazakhstan', city: 'Almaty'}},
-        {id: 3, followed: true, avatar: 'https://i.pinimg.com/564x/b2/81/ff/b281ff729f1b0b37d37d0d4400635912.jpg', name: 'Alimzhan', status: 'i love Meme', location: {country: 'Kazakhstan', city: 'Semey'}},
-        {id: 4, followed: true, avatar: 'https://i.pinimg.com/564x/b2/81/ff/b281ff729f1b0b37d37d0d4400635912.jpg', name: 'Alisher', status: 'i love Zhanel', location: {country: 'Kazakhstan', city: 'Semey'}},
-    ],
+    users:[]
 }
 
 const usersReducer = (state = initialState, action) => {
